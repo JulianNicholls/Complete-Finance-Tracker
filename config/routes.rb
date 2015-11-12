7007fb69e963bb8f148a14abaac57b15b97c6a07
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-12 17:08:49 +0000

    Add friends list and minimal search

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
-  get 'my_portfolio',  to: 'users#my_portfolio'
-  get 'search_stocks', to: 'stocks#search'
-  get 'my_friends',    to: 'users#my_friends'
+  get  'my_portfolio',   to: 'users#my_portfolio'
+  get  'search_stocks',  to: 'stocks#search'
+  get  'my_friends',     to: 'users#my_friends'
+  get  'search_friends', to: 'user#search'
+  post 'add_friend',     to: 'user#add_friend'
 end
